4facdd6804691c53ffe455090b065ce501d05c78
Sébastien Vérité <sebastien.verite@gmail.com>
2017-12-06 11:09:45 -0400

    Update TranslatableSubscriber.php

--- src/ORM/Translatable/TranslatableSubscriber.php
+++ src/ORM/Translatable/TranslatableSubscriber.php
@@ -313,6 +313,10 @@ class TranslatableSubscriber extends AbstractSubscriber
         $em            = $eventArgs->getEntityManager();
         $entity        = $eventArgs->getEntity();
         $classMetadata = $em->getClassMetadata(get_class($entity));
+        
+        if (!$this->isTranslatable($classMetadata)) {
+             return;
+        }
 
         if (!$this->getClassAnalyzer()->hasMethod($classMetadata->reflClass, 'setCurrentLocale')) {
             return;
